018dde89f9040cc2a1da1bb35ee5bfd78e2723c1
QueryWala <ization.in@gmail.com>
2026-07-26 17:02:27 +0000

    Indexpage changes

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -48,11 +48,8 @@ export default defineAstroPaperConfig({
   ],
 
   shareLinks: [
-    { name: "whatsapp", url: "https://wa.me/?text=" },
-    { name: "facebook", url: "https://www.facebook.com/sharer.php?u=" },
-    { name: "x", url: "https://x.com/intent/post?url=" },
-    { name: "telegram", url: "https://t.me/share/url?url=" },
-    { name: "pinterest", url: "https://pinterest.com/pin/create/button/?url=" },
-    { name: "mail", url: "mailto:?subject=See%20this%20post&body=" },
-  ],
+  { name: "linkedin", url: "https://www.linkedin.com/sharing/share-offsite/?url=" },
+  { name: "whatsapp", url: "https://wa.me/?text=" },
+  { name: "mail", url: "mailto:?subject=See%20this%20article&body=" },
+],
 });
